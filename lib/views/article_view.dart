71239444af98be44c2ed30600dd0072c44b4cd64
vedantsinh <68774584+vedantsinh@users.noreply.github.com>
2021-04-21 09:23:09 +0530

    first commit

---
+++ lib/views/article_view.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleView extends StatefulWidget {
+  final String blogUrl;
+  ArticleView({this.blogUrl});
+  @override
+  _ArticleViewState createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(children: <Widget>[
+          Text("Flutter"),
+          Text("News", style: TextStyle(color: Colors.blue))
+        ]),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.blogUrl,
+          onWebViewCreated: ((WebViewController webViewController) {
+            _completer.complete(webViewController);
+          }),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/category_news.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/helper/news.dart';
+import 'package:news_app/models/article_model.dart';
+
+import 'article_view.dart';
+import 'home.dart';
+
+class CategoryNews extends StatefulWidget {
+  final String category;
+  CategoryNews({this.category});
+  @override
+  _CategoryNewsState createState() => _CategoryNewsState();
+}
+
+class _CategoryNewsState extends State<CategoryNews> {
+  List<ArticleModel> articles = new List<ArticleModel>();
+  bool _loading = true;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getCategoryNews();
+  }
+
+  getCategoryNews() async {
+    CategoryNewsClass news = CategoryNewsClass();
+    await news.getNews(widget.category);
+    articles = news.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(children: <Widget>[
+            Text("Flutter"),
+            Text("News", style: TextStyle(color: Colors.blue))
+          ]),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: _loading
+            ? Center(
+                child: Container(
+                  child: CircularProgressIndicator(),
+                ),
+              )
+            : SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Column(children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.only(top: 16),
+                        child: ListView.builder(
+                            itemCount: articles.length,
+                            shrinkWrap: true,
+                            physics: ClampingScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              return BlogTile(
+                                imageUrl: articles[index].urlToImage,
+                                title: articles[index].title,
+                                desc: articles[index].description,
+                                url: articles[index].url,
+                              );
+                            }))
+                  ]),
+                ),
+              ));
+  }
+}
+
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc, url;
+  BlogTile(
+      {@required this.imageUrl,
+      @required this.title,
+      @required this.desc,
+      @required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ArticleView(
+                      blogUrl: url,
+                    )));
+      },
+      child: Container(
+          margin: EdgeInsets.only(bottom: 16),
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Image.network(imageUrl)),
+              Text(title,
+                  style: TextStyle(fontSize: 17, color: Colors.black87)),
+              SizedBox(
+                height: 8,
+              ),
+              Text(desc, style: TextStyle(color: Colors.grey))
+            ],
+          )),
+    );
+  }
+}
